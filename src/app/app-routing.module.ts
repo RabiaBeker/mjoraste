ca60d90846f422029ea1a0c81305d93de740bd8b
Volkan Görer <49448200+volkangorer@users.noreply.github.com>
2023-07-17 18:52:42 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomePageComponent} from "./home-page/home-page.component";
-import {RegisterComponent} from "./register/register.component";
 import {ProductDetailPageComponent} from "./product-detail-page/product-detail-page.component";
 import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
 
 const routes: Routes = [
   {
     path: "", component: HomePageComponent
-  },
-  {
-    path: "register", component: RegisterComponent
   },
   // {
   //   path: "login", component: LoginComponent
