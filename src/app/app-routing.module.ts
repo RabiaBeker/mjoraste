ff610ebada81fcb6e37b86ca38a0f985cbb66f52
abdullahbeker <abdullah.beker@pia-team.com>
2023-07-17 18:08:43 +0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./home-page/home-page.component";
+import {RegisterComponent} from "./register/register.component";
+import {ProductDetailPageComponent} from "./product-detail-page/product-detail-page.component";
+import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: HomePageComponent
+  },
+  {
+    path: "register", component: RegisterComponent
+  },
+  // {
+  //   path: "login", component: LoginComponent
+  // }
+  {
+    path: "detail", component: ProductDetailPageComponent
+  },
+  {
+    path: "shoppingCart", component: ShoppingCartComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from "@angular/material/input";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {ProductDetailPageComponent} from "./product-detail-page/product-detail-page.component";
+import { CarouselComponent } from './product-detail-page/carousel/carousel.component';
+import {MatSelectModule} from "@angular/material/select";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatRadioButton, MatRadioModule} from "@angular/material/radio";
+import { NavbarComponent } from './navbar/navbar.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    HomePageComponent
+    HomePageComponent,
+    ProductDetailPageComponent,
+    CarouselComponent,
+    NavbarComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
@@ -27,8 +37,11 @@ import {MatToolbarModule} from "@angular/material/toolbar";
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
-    MatToolbarModule
-
+    MatToolbarModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
